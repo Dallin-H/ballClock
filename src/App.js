6b54d1457e49837b3262114566e2bb959b148e4c
Dallin-H <dallinhyde@gmail.com>
2019-05-17 19:18:57 -0600

    clean up the code

--- src/App.js
+++ src/App.js
@@ -38,20 +38,16 @@ export default class App extends Component {
         queueCopy.push(index + 1);
       }
 
-      // action
-      // add 1 to timer
-      // remove first ballBearing from trackQueue and place it on the end of trackMinuteOne.
-      // if trackMinuteOne length === 5, trigger dropMinuteOne.
       let action = () => {
         let ball = queueCopy.shift();
         trackMinuteOne.push(ball);
+        
+
+
       };
 
-      // dropMinuteOne
-      // remove 1st item from trackMinuteOne and place it at the end of trackMinuteFive.
-      // empty trackMinuteOne in the correct order into trackQueue.
-      // if trackMinuteFive length is === 12, trigger dropMinuteFive.
       let dropMinuteOne = () => {
+
         let dropBall = trackMinuteOne.pop();
         trackMinuteFive.push(dropBall);
 
@@ -61,11 +57,8 @@ export default class App extends Component {
         }
       };
 
-      // dropMinuteFive
-      // remove 1st item from trackMinuteOne and place it at the end of trackHour.
-      // empty trackMinuteFive in the correct order into trackQueue.
-      // if trackHour length === 12, trigger dropHour.
       let dropMinuteFive = () => {
+
         let dropBall = trackMinuteFive.pop();
         trackHour.push(dropBall);
 
@@ -75,10 +68,6 @@ export default class App extends Component {
         }
       };
 
-      // drop hour
-      // remove all and add them to trackQueue in correct order.
-      // check trackQueue and compare it to original Queue for solution.
-      // if solved, run calculator. if not, run action.
       let dropHour = () => {
         timer++;
         for (let index = trackHour.length - 1; index >= 0; index--) {
@@ -86,48 +75,43 @@ export default class App extends Component {
           queueCopy.push(ball);
         }
 
-
         let equalYetLocal = false;
         let checkEqual = () => {
           if (queueCopy[0] !== 1) {
-              equalYetLocal =  false;
+            equalYetLocal = false;
           } else {
-            console.log('starts @ 1')
             if (JSON.stringify(trackQueue) === JSON.stringify(queueCopy)) {
               equalYetLocal = true;
             } else {
               equalYetLocal = false;
             }
           }
-        }
+        };
 
         checkEqual();
         equalYetGlobal = equalYetLocal;
       };
 
-      // calculate
-      // timer is holding a sum of minutes. Calculate will determining how many days will pass
-      // to return the queue to it's original state.
       let calculate = () => {
+
         let roundedSum = Math.floor(timer / 2);
 
         this.setState({ result: roundedSum + " days" });
       };
 
-
-      // do...while loop is running a conditinal statement before triggering the next action.
       do {
         if (trackHour.length === 12) {
-          console.log("Hour Drop");
           dropHour();
         } else if (trackMinuteFive.length === 12) {
-          // console.log("5 Minute Drop");
           dropMinuteFive();
         } else if (trackMinuteOne.length === 5) {
-          // console.log("Minute Drop");
           dropMinuteOne();
         } else {
-          // console.log("Action");
+          console.log('q: ' + queueCopy)
+          console.log('t1: ' + trackMinuteOne)
+          console.log('t5: ' + trackMinuteFive)
+          console.log('th: ' + trackHour)
+          console.log('-----------------')
           action();
         }
       } while (equalYetGlobal === false);
@@ -139,9 +123,8 @@ export default class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <p>30 -> 45 days</p>
-          <p>45 -> 378 days</p>
-          <p></p>
+          <h3>Welcome to the Ball Clock Calculator!</h3>
+
           <input
             type="number"
             placeholder="27-127"
